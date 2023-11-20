4ed62bd924a32b17f5c6373a2a9a0d54493e41a8
Sergey Anisov <strelas70@gmail.com>
2023-11-20 21:09:05 +0500

    add some comments

--- lib/fullscreen_image_viewer.dart
+++ lib/fullscreen_image_viewer.dart
@@ -6,7 +6,8 @@ import 'package:fullscreen_image_viewer/fullscreen_viewer.dart';
 class FullscreenImageViewer {
   FullscreenImageViewer._();
 
-  /// Child should be a Hero widget for transition animation
+  /// [child] should be a Hero widget for transition animation
+  /// [closeWidget] is content of button at top right corner that closes viewer
   static Future<void> open({
     required BuildContext context,
     required Widget child,
